0192ed16a96b1e7e546b7f17239d2d4ee2a00cc2
Abhimaneu N S <abhimaneu2001@gmail.com>
2023-07-31 23:49:17 +0530

    Update app.py

--- New/app.py
+++ New/app.py
@@ -3,4 +3,25 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
-print("helo")
+# Create Flask app
+app = Flask(__name__)
+
+# API endpoint for image processing
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    # Get the image file from the request
+    image_file = request.files['image']
+
+    # Load the image using PIL
+    image = Image.open(image_file)
+
+    # Preprocess the image (resize, normalize, etc.)
+    # ...
+
+    # Perform inference using the loaded model
+    # ...
+
+    # Process the model output and return the results
+    # ...
+
+    return jsonify(results)
